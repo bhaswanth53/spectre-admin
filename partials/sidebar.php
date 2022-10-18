2374b195480175d74407b69358b00e1edc6700c8
bhaswanth53 <bhaswanth22@gmail.com>
2022-10-18 18:32:00 +0530

    First Commit

---
+++ partials/sidebar.php
@@ -0,0 +1,34 @@
+<aside class="leftbar" id="left-bar">
+    <ul class="nav">
+        <li class="nav-item active">
+            <a href="">
+                <i class="fa fa-dashboard"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="">
+                <i class="fa fa-address-card"></i>
+                <span>Experience</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="">
+                <i class="fa fa-code"></i>
+                <span>Projects</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="">
+                <i class="fa fa-certificate"></i>
+                <span>Skills</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="">
+                <i class="fa fa-edit"></i>
+                <span>Posts</span>
+            </a>
+        </li>
+    </ul>
+</aside>
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Spectre Admin
+
+An admin panel UI designed with <a href="https://picturepan2.github.io/spectre/">spectre.css</a>
